f06ad232407de4d4545d9a59c42f106f0f1a380e
Rein Henrichs <rein.henrichs@gmail.com>
2015-06-27 12:33:24 -0700

    Extract Pandoc processor to library and provide non-IO version

--- pandoc-crossref.hs
+++ pandoc-crossref.hs
@@ -10,17 +10,4 @@ import           Text.Pandoc.Crossref.Util.Options
 import           Text.Pandoc.Crossref.Util.Settings
 
 main :: IO ()
-main = toJSONFilter go
-
-go :: Maybe Format -> Pandoc -> IO Pandoc
-go fmt (Pandoc meta bs) = do
-  dtv <- getSettings meta
-  let
-    doWalk =
-      bottomUpM (codeBlockCaptions opts) bs
-      >>= walkM (replaceBlocks opts)
-      >>= bottomUpM (replaceRefs opts)
-      >>= bottomUpM (listOf opts)
-    opts = getOptions dtv fmt
-    meta' = modifyMeta opts dtv
-  return $ Pandoc meta' $ evalState doWalk def
+main = toJSONFilter process
--- src/Text/Pandoc/Crossref.hs
+++ src/Text/Pandoc/Crossref.hs
@@ -1,7 +1,35 @@
-module Text.Pandoc.Crossref ( module X ) where
+module Text.Pandoc.Crossref (process, processWith, module X) where
 
-import           Text.Pandoc.Crossref.Accessors as X
-import           Text.Pandoc.Crossref.Blocks    as X
-import           Text.Pandoc.Crossref.List      as X
-import           Text.Pandoc.Crossref.Refs      as X
-import           Text.Pandoc.Crossref.Types     as X
+import           Control.Monad.State
+import           Data.Monoid                                 ((<>))
+import           Text.Pandoc
+import           Text.Pandoc.JSON
+import           Text.Pandoc.Walk
+
+-- For reexport
+import           Text.Pandoc.Crossref.Accessors              as X
+import           Text.Pandoc.Crossref.Blocks                 as X
+import           Text.Pandoc.Crossref.List                   as X
+import           Text.Pandoc.Crossref.Refs                   as X
+import           Text.Pandoc.Crossref.Types                  as X
+
+import           Text.Pandoc.Crossref.Util.CodeBlockCaptions
+import           Text.Pandoc.Crossref.Util.ModifyMeta
+import           Text.Pandoc.Crossref.Util.Options
+import           Text.Pandoc.Crossref.Util.Settings
+
+processWith :: Meta -> Maybe Format -> Pandoc -> Pandoc
+processWith dtv fmt (Pandoc meta bs) = Pandoc meta' $ evalState doWalk def
+  where
+    doWalk =
+      bottomUpM (codeBlockCaptions opts) bs
+      >>= walkM (replaceBlocks opts)
+      >>= bottomUpM (replaceRefs opts)
+      >>= bottomUpM (listOf opts)
+    opts = getOptions (dtv <> meta) fmt
+    meta' = modifyMeta opts dtv
+
+process :: Maybe Format -> Pandoc -> IO Pandoc
+process fmt pandoc@(Pandoc meta bs) = do
+  dtv <- getSettings meta
+  return (processWith dtv fmt pandoc)
